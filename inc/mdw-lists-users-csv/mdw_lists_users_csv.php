689d681a2555d2824b5652028d45d78cf8a290ac
Darwin Roa <darwinmichaelroamora@gmail.com>
2025-02-12 18:48:19 -0500

    feat(listado motos): Agrega nuevas columnas al listado de motos

--- inc/mdw-lists-users-csv/mdw_lists_users_csv.php
+++ inc/mdw-lists-users-csv/mdw_lists_users_csv.php
@@ -12,7 +12,7 @@ function mdw_lists_users_csv_function()
         // Crea un archivo CSV
         $file = fopen('wp-content/themes/hub-child/inc/mdw-lists-users-csv/users.csv', 'w');
         // Encabezado del archivo CSV
-        fputcsv($file, array('Nombre', 'Apellido', 'Correo electrónico', 'Marca de Moto', 'Referencia', 'Modelo')); // Encabezado del archivo CSV
+        fputcsv($file, array('Nombre', 'Apellido', 'Genero', 'Fecha de nacimiento', 'Correo electronico', 'Telefono', 'Marca de Moto', 'Referencia', 'Modelo', 'Placa')); // Encabezado del archivo CSV
         // Recorre todos los usuarios
         foreach ($users as $user) {
             // Obtiene el ID del usuario
@@ -23,6 +23,12 @@ function mdw_lists_users_csv_function()
             $last_name = $user->last_name;
             // Obtiene el correo electrónico del usuario
             $email = $user->user_email;
+            // Obtiene la fecha de nacimiento del usuario
+            $birth_day = get_user_meta($id, 'fecha_nacimiento', true);
+            // Obtiene el género del usuario
+            $gender = get_user_meta($id, 'genero', true);
+            // Obtiene el número de celular del usuario
+            $phone = get_user_meta($id, 'numero_telefono', true);
             // Obtener los valores ya registrados de "motos" en el usuario
             $motosRegistradas = get_user_meta($id, 'motos', true);
             // Asegurarnos de que $motosRegistradas sea un array
@@ -38,17 +44,23 @@ function mdw_lists_users_csv_function()
                 $referencia = $moto['referencia'];
                 // Obtiene el modelo de la moto
                 $modelo = $moto['modelo'];
+                // Obtiene el número de placa de la moto
+                $placa = $moto['placa'];
                 // Escribe los datos del usuario en el archivo CSV
-                fputcsv($file, array($first_name, $last_name, $email, $marca, $referencia, $modelo));
+                fputcsv($file, array($first_name, $last_name, $gender, $birth_day, $email, $phone, $marca, $referencia, $modelo, $placa));
                 // Agrega los datos del usuario a la tabla HTML
                 $htmlTBody .= "
                     <tr>
                         <td>$first_name</td>
                         <td>$last_name</td>
+                        <td>$gender</td>
+                        <td>$birth_day</td>
                         <td>$email</td>
+                        <td>$phone</td>
                         <td>$marca</td>
                         <td>$referencia</td>
                         <td>$modelo</td>
+                        <td>$placa</td>
                     </tr>
                 ";
             }
@@ -63,10 +75,14 @@ function mdw_lists_users_csv_function()
                     <tr>
                         <th>Nombre</th>
                         <th>Apellido</th>
+                        <th>Género</th>
+                        <th>Fecha Nacimiento</th>
                         <th>Correo electrónico</th>
+                        <th>Teléfono</th>
                         <th>Marca de Moto</th>
                         <th>Referencia</th>
                         <th>Modelo</th>
+                        <th>Placa</th>
                     </tr>
                 </thead>
                 <tbody>
